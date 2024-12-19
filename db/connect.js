416c5352be26510cc4f321af175e6c6501c42922
go2023 <tetyanago2018@gmail.com>
2024-12-19 15:22:22 -0800

    Commented lines were deleted

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,5 @@
 // connect.js
 
-// const mongoose = require('mongoose')
-//
-// const connectDB = (url) => {
-//     return mongoose.connect(url, {
-//         useNewUrlParser: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false,
-//         useUnifiedTopology: true,
-//     })
-// }
-//
-// module.exports = connectDB
-
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
