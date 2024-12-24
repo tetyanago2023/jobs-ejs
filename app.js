312d9a789acc2bf5a52d1dae2f074a9459235a81
tetyanago2023 <137588868+tetyanago2023@users.noreply.github.com>
2024-12-24 11:43:59 -0800

    My Answer for Lesson12/Week14  - Server Side Rendering with EJS - Assignment (#1)
    * Dependencies were installed in package.json, directories db and models (with files) were created
    
    * "body-parser" was added to package.json, app.js and views .ejs were created
    
    * Session middleware and some errors handling were implemented
    
    * Flash messages functionality was implemented

---
+++ app.js
@@ -0,0 +1,90 @@
+// app.js
+
+const express = require("express");
+require("express-async-errors");
+require("dotenv").config(); // Load environment variables
+const session = require("express-session");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(require("body-parser").urlencoded({ extended: true }));
+
+// Session middleware setup
+const MongoDBStore = require("connect-mongodb-session")(session);
+const url = process.env.MONGO_URI;
+
+const store = new MongoDBStore({
+    // may throw an error, which won't be caught
+    uri: url,
+    collection: "mySessions",
+});
+store.on("error", function (error) {
+    console.log(error);
+});
+
+const sessionParms = {
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: store,
+    cookie: { secure: false, sameSite: "strict" },
+};
+
+if (app.get("env") === "production") {
+    app.set("trust proxy", 1); // trust first proxy
+    sessionParms.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sessionParms));
+
+app.use(require("connect-flash")());
+
+app.get("/secretWord", (req, res) => {
+    if (!req.session.secretWord) {
+        req.session.secretWord = "syzygy"; // Initialize with default if not set
+    }
+
+    // Retrieve flash messages
+    const errors = req.flash("error");
+    const info = req.flash("info");
+
+    res.render("secretWord", {
+        secretWord: req.session.secretWord,
+        errors,
+        info
+    });
+});
+
+app.post("/secretWord", (req, res) => {
+    try {
+        const input = req.body.secretWord;
+
+        if (typeof input === "string" && input.toUpperCase()[0] === "P") {
+            req.flash("error", "That word won't work!");
+            req.flash("error", "You can't use words that start with 'P'.");
+        } else {
+            // Ensure secretWord is always a string
+            req.session.secretWord =
+                typeof input === "string" ? input : JSON.stringify(input);
+            req.flash("info", "The secret word was changed.");
+        }
+    } catch (err) {
+        req.flash("error", "Invalid input.");
+    }
+    res.redirect("/secretWord");
+});
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        app.listen(PORT, () =>
+            console.log(`Server running on http://localhost:${PORT}`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
