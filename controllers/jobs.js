85794a01bfecd0dbe34d64c42f0d26056cbf6301
go2023 <tetyanago2018@gmail.com>
2024-12-28 10:15:22 -0800

    Code adjustment for testing by Postman

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -9,7 +9,7 @@ const getAllJobs = async (req, res, next) => {
         }
 
         const page = parseInt(req.query.page, 10) || 1;
-        const limit = parseInt(req.query.limit, 10) || 5;
+        const limit = parseInt(req.query.limit, 10) || 10;
         const skip = (page - 1) * limit;
 
         const companyFilter = req.query.company || '';
--- passport/passportInit.js
+++ passport/passportInit.js
@@ -1,4 +1,4 @@
-// passportInit.js
+// passport/passportInit.js
 
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
